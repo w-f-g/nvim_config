dec42272f7bc6c539f0203f36a89087ebe6abb5d
w-f-g <1035550175@qq.com>
2024-08-18 12:59:16 +0800

    feat: 结构优化

--- lua/community.lua
+++ lua/community.lua
@@ -1,4 +1,4 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+-- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
@@ -7,6 +7,31 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  { import = "plugins.pack_lua" },
+  -- { import = "plugins.lua" },
   -- import/override with your plugins folder
+  -- { import = 'astrocommunity.pack.' },
+  { import = "astrocommunity.lsp.nvim-lsp-file-operations" },
+  { import = "astrocommunity.motion.flash-nvim" },
+  { import = "astrocommunity.utility.noice-nvim" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+
+  -- language
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.sql" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.dart" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.prisma" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.tailwindcss" },
+  { import = "astrocommunity.pack.typescript-deno" },
+  { import = "astrocommunity.pack.typescript-all-in-one" },
 }
