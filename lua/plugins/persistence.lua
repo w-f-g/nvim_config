6c2644eef20934126bfbbcbd09ca67b4ad38711d
w-f-g <1035550175@qq.com>
2024-09-28 20:17:36 +0800

    update: persistence快捷键修改

--- lua/plugins/persistence.lua
+++ lua/plugins/persistence.lua
@@ -8,8 +8,8 @@ return {
     opts = {},
     -- stylua: ignore
     keys = {
-      { prefix .. "s", function() require("persistence").load() end, desc = "Restore Session" },
-      { prefix .. "S", function() require("persistence").select() end,desc = "Select Session" },
+      -- { prefix .. "s", function() require("persistence").load() end, desc = "Restore Session" },
+      { prefix .. "s", function() require("persistence").select() end,desc = "Select Session" },
       { prefix .. "l", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
       { prefix .. "d", function() require("persistence").stop() end, desc = "Don't Save Current Session" },
     },
