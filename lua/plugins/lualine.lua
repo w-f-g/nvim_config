83d422696c5cdbb74a753ad9c0282aa8847e2034
w-f-g <1035550175@qq.com>
2024-11-19 13:07:20 +0800

    change: lualine配置修改

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,3 +1,31 @@
+local colors = {
+  blue = "#80a0ff",
+  cyan = "#79dac8",
+  black = "#080808",
+  white = "#c6c6c6",
+  red = "#ff5189",
+  violet = "#d183e8",
+  grey = "#303030",
+}
+
+local bubbles_theme = {
+  normal = {
+    a = { fg = colors.black, bg = colors.violet },
+    b = { fg = colors.white, bg = colors.grey },
+    c = { fg = colors.white },
+  },
+
+  insert = { a = { fg = colors.black, bg = colors.blue } },
+  visual = { a = { fg = colors.black, bg = colors.cyan } },
+  replace = { a = { fg = colors.black, bg = colors.red } },
+
+  inactive = {
+    a = { fg = colors.white, bg = colors.black },
+    b = { fg = colors.white, bg = colors.black },
+    c = { fg = colors.white },
+  },
+}
+
 return {
   "nvim-lualine/lualine.nvim",
   -- event = "VeryLazy",
@@ -5,9 +33,10 @@ return {
   opts = {
     options = {
       icons_enabled = true,
-      theme = "auto",
+      theme = bubbles_theme,
       component_separators = { left = "", right = "" },
-      section_separators = { left = "", right = "" },
+      section_separators = { left = "", right = "" },
+      -- section_separators = { left = "", right = "" },
       disabled_filetypes = {
         statusline = {},
         winbar = {},
@@ -24,14 +53,29 @@ return {
     },
     sections = {
       lualine_a = { "mode" },
-      lualine_b = { "branch", "diff", "diagnostics" },
+      lualine_b = {
+        "branch",
+        -- "diff",
+        {
+          "diagnostics",
+          sources = { "nvim_diagnostic" },
+          symbols = { error = " ", warn = " ", info = " " },
+          diagnostics_color = {
+            error = { fg = "#ec5f67" },
+            warn = { fg = "#ecbe7b" },
+            info = { fg = "#51afef" },
+          },
+        },
+      },
       lualine_c = { "filename" },
       lualine_x = { "filetype" },
       lualine_y = {},
-      lualine_z = { {
-        "datetime",
-        style = "%H:%M",
-      } },
+      lualine_z = {
+        {
+          "datetime",
+          style = "%H:%M",
+        },
+      },
     },
     inactive_sections = {
       lualine_a = {},
